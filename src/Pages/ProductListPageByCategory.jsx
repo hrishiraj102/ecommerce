976f9fa1059fb8232671355b042535e6884c2f50
Hrishiraj Sawan <hrishiraj102@gmail.com>
2023-08-29 02:39:34 +0530

    fix cat

--- src/Pages/ProductListPageByCategory.jsx
+++ src/Pages/ProductListPageByCategory.jsx
@@ -2,17 +2,17 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ProductListPageByCategory =() =>{
-    const {cat} = useParams();
-    const [productList1,setProductList1] = useState([])
-    useEffect(()=>{
-        fetch(`https://dummyjson.com/products/category/${cat}`)
-        .then(res => res.json())
-        .then((data) => {
+    // const {cat} = useParams();
+    // const [productList1,setProductList1] = useState([])
+    // useEffect(()=>{
+    //     fetch(`https://dummyjson.com/products/category/${cat}`)
+    //     .then(res => res.json())
+    //     .then((data) => {
            
-            setProductList1(data.products)
-            console.log(data.products);
-        })
-    },[]);
+    //         setProductList1(data.products)
+    //         console.log(data.products);
+    //     })
+    // },[]);
 
 
     return(
